d6cc19916cdb474affe6e6d42571d0c888177575
Devin Humbert <devin.humbert@gmail.com>
2013-06-24 22:54:28 -0700

    Initial commit

---
+++ pyasm.py
@@ -0,0 +1,72 @@
+import sys
+import operator
+
+class Parser:
+    def __init__(self, content):
+        self.registers = {}
+        self.accumulator = 0
+        self.tokens = content.read().split()
+
+    def parse(self):
+        while self.tokens:
+            tok = self.next_token()
+
+            if tok == 'get':
+                self._op_get()
+            elif tok == 'store':
+                register = self.next_token()
+                self._op_store(register)
+            elif tok == 'put':
+                self._op_put()
+            elif tok == 'add':
+                register = self.next_token()
+                self._op_math(operator.add, register)
+            elif tok == 'sub':
+                register = self.next_token()
+                self._op_math(operator.sub, register)
+            elif tok == 'mul':
+                register = self.next_token()
+                self._op_math(operator.mul, register)
+            elif tok == 'div':
+                register = self.next_token()
+                self._op_math(operator.truediv, register)
+
+    def next_token(self):
+        if self.tokens:
+            nexttok = self.tokens[0]
+            self.tokens = self.tokens[1:]
+            return nexttok
+
+    def peek(self):
+        try:
+            return self.tokens[0]
+        except IndexError:
+            return None
+
+    def stringify(self, value):
+        if value[0] is '"' or value[0] is "'":
+            if value[-1] is '"' or value[-1] is "'":
+                return value[1:-1]
+
+    def symbol_value(self, name):
+        return symbols[name]
+
+    def _op_get(self):
+        self.accumulator = int(input("input > "))
+
+    def _op_store(self, register):
+        self.registers[register] = self.accumulator
+
+    def _op_put(self):
+        print(self.accumulator)
+
+    def _op_math(self, op, register):
+        self.accumulator = op(self.registers[register], self.accumulator)
+
+
+def main(pyAsmFile):
+    with open(pyAsmFile) as readFile:
+        parser = Parser(readFile)
+        parser.parse()
+
+main(sys.argv[1])
---
+++ sample.pasm
@@ -0,0 +1,17 @@
+get
+store a
+get
+add a
+put
+store b
+get
+sub b
+put
+store c
+get
+mul c
+put
+store d
+get
+div d
+put
